fc09c2b79dcee0eef4935f6b134f751aeb2dae99
miyabifox <miyabi.s@live.jp>
2016-12-16 11:27:03 +0900

    sumtest

--- tests/SumTest.php
+++ tests/SumTest.php
@@ -27,6 +27,18 @@ class SumTest extends TestCase
 		$this->assertEquals($this->HelloController->sum(10,10),20);
     }
 	
+public function sum($i, $j){
+  if( !is_int($i) || !is_int($j) ) return;
+  if( is_float($i) || is_float($j)) return;
+  if( $i > 999 || $j > 999 ) return;
+  if( $i < -999 || $j < -999 ) return;
+  $result  = $i + $j;
+  if( $result == 777 ) return "Happy";
+  if( $result > 1998 ) return;
+  if( $result < -1998 ) return;
+  return $result;
+  }
+	
 	//public function testtext()		
     //{
     //    $this->visit('/')
